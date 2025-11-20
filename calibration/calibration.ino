5e054eb57ea78902eac2f5321b2a9dc28fb60f4d
Andrew Phung <andrewphung@berkeley.edu>
2025-11-19 17:22:36 -0800

    one way - 8900 counts

--- calibration/calibration.ino
+++ calibration/calibration.ino
@@ -2,31 +2,32 @@
 #include <ESP32Encoder.h>
 
 // --------- MOTOR PINS ---------
-#define MOTOR_IN1 26   // same as your BIN_1
-#define MOTOR_IN2 25   // same as your BIN_2
+#define MOTOR_IN1 26
+#define MOTOR_IN2 25
 
 // --------- ENCODER SETUP ---------
 ESP32Encoder encoder;
 
 // --------- MOTOR TEST PARAMETERS ---------
-const int MOTOR_SPEED = 255;      // full speed
-const int DRIVE_TIME_MS = 3000;   // motor runs for 3 sec
-const int REST_TIME_MS  = 2000;   // pause for 2 sec
+const int MOTOR_SPEED = 255;
+const int DRIVE_TIME_MS = 3000;
+const int REST_TIME_MS  = 2000;
+
+// --------- SAMPLE SPEED ---------
+const int SAMPLE_DELAY_MS = 5;   // <- was 100 ms, now MUCH faster
 
 void setup() {
   Serial.begin(115200);
   delay(500);
-  Serial.println("=== MOTOR + ENCODER COMBINED TEST ===");
+  Serial.println("=== MOTOR + ENCODER HIGH-RES TEST ===");
 
-  // ----- MOTOR PINS -----
   pinMode(MOTOR_IN1, OUTPUT);
   pinMode(MOTOR_IN2, OUTPUT);
   digitalWrite(MOTOR_IN1, LOW);
   digitalWrite(MOTOR_IN2, LOW);
 
-  // ----- ENCODER SETUP -----
   ESP32Encoder::useInternalWeakPullResistors = puType::up;
-  encoder.attachHalfQuad(27, 33);   // your working pins
+  encoder.attachHalfQuad(27, 33);
   encoder.setCount(0);
 
   Serial.println("Encoder initialized.");
@@ -45,7 +46,7 @@ void loop() {
   while (millis() - start < DRIVE_TIME_MS) {
     Serial.print("Encoder count = ");
     Serial.println((int32_t)encoder.getCount());
-    delay(100);
+    delay(SAMPLE_DELAY_MS);
   }
 
   // ---- PHASE 2: Motor stop ----
@@ -57,8 +58,6 @@ void loop() {
   while (millis() - start < REST_TIME_MS) {
     Serial.print("Encoder count = ");
     Serial.println((int32_t)encoder.getCount());
-    delay(100);
+    delay(SAMPLE_DELAY_MS);
   }
-
-  // Loop repeats automatically
 }
